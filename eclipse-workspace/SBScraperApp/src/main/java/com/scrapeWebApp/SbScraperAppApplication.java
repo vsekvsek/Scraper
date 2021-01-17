433ca7dfb6059287438f5a282d06a82b6155479b
steve e kelly <stevekelly@hotmail.com>
2021-01-17 13:33:06 -0800

    wip

--- eclipse-workspace/SBScraperApp/src/main/java/com/scrapeWebApp/SbScraperAppApplication.java
+++ eclipse-workspace/SBScraperApp/src/main/java/com/scrapeWebApp/SbScraperAppApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan; 
 
 @SpringBootApplication
 //@ComponentScan(value = "com.service" useDefaultFilters = false)
 @ComponentScan({"com.service","com.controller","com.repository"})
 @EntityScan(basePackages = "com.spring.bean")
+//@ImportResource({"classpath*:applicationContext.xml"})
 public class SbScraperAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
